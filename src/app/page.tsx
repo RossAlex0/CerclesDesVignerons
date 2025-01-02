46754c06481dfb4bee7260443765aa7abb7f8de2
Rossignol Alex <alexnw33910@gmail.com>
2025-01-03 00:28:07 +0100

    Update app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,55 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import Image from "next/image";
 
 import ParallaxBackground from "@/components/ParallaxBackground";
+import Footer from "@/components/Footer/Index";
+import Header from "@/components/Header/Index";
 
 import "@/styles/home.css";
 
 export default function Home() {
   const router = useRouter();
 
+  const [viewContact, setViewContact] = useState(false);
+
   return (
-    <ParallaxBackground>
-      <section className="home">
-        <div className="home_container">
-          <Image
-            src="/logo/simple_white_logo.svg"
-            alt="cdv"
-            width={200}
-            height={200}
-          />
-          <h1>
-            Savourez l&apos;authenticité <br />
-            et la richesse des grands vins.
-          </h1>
-          <div className="home_container_btn">
-            <button type="button" onClick={() => router.push("/wine")}>
-              Découvrez nos vins
-            </button>
-            <button type="button">Contactez-nous</button>
+    <>
+      <Header viewContact={viewContact} setViewContact={setViewContact} />
+      <ParallaxBackground>
+        <section className="home">
+          <div
+            className="home_container"
+            style={
+              viewContact
+                ? { opacity: 0, transition: "all 1s ease-out" }
+                : { opacity: 1, transition: "all 4s ease-in" }
+            }
+          >
+            <Image
+              src="/logo/simple_white_logo.svg"
+              alt="cdv"
+              width={200}
+              height={200}
+            />
+            <h1>
+              Savourez l&apos;authenticité <br />
+              et la richesse des grands vins.
+            </h1>
+            <div className="home_container_btn">
+              <button type="button" onClick={() => router.push("/wine")}>
+                Découvrez nos vins
+              </button>
+              <button type="button" onClick={() => setViewContact(true)}>
+                Contactez-nous
+              </button>
+            </div>
           </div>
-        </div>
-      </section>
-    </ParallaxBackground>
+        </section>
+      </ParallaxBackground>
+      <Footer viewContact={viewContact} />
+    </>
   );
 }
