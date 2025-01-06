3a07e02eef2e6ab5cbfe1d8e5e2e87f6af477435
Rossignol Alex <alexnw33910@gmail.com>
2025-01-07 00:52:15 +0100

    Fix error with window

--- src/services/useWindowWidth.ts
+++ src/services/useWindowWidth.ts
@@ -1,22 +1,23 @@
 import { useState, useEffect } from "react";
 
 export default function useWindowWidth(): number {
-  const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+  const [screenWidth, setScreenWidth] = useState<number>(0);
 
   useEffect(() => {
-    const getWindowWidth = () => {
-      if (window !== undefined) {
+    if (typeof window !== "undefined") {
+      const getWindowWidth = () => {
         setScreenWidth(window.innerWidth);
-      }
-    };
+      };
 
-    getWindowWidth();
+      getWindowWidth();
 
-    window.addEventListener("resize", getWindowWidth);
+      window.addEventListener("resize", getWindowWidth);
 
-    return () => {
-      window.removeEventListener("resize", getWindowWidth);
-    };
+      return () => {
+        window.removeEventListener("resize", getWindowWidth);
+      };
+    }
   }, []);
+
   return screenWidth;
 }
